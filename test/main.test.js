c83dc73d3bed5716084245e4f3935620c3971dd2
kaihengann <annkaiheng@gmail.com>
2019-06-01 12:49:04 +0800

    Add first 3 tests

--- test/main.test.js
+++ test/main.test.js
@@ -1,10 +1,43 @@
 const processPayments = require("../src/main");
+const paymentService = require("../src/paymentService");
+const queueService = require("../src/queueService");
+const makePayment = paymentService.makePayment;
+const refundPayment = paymentService.refundPayment;
+const generateQueue = queueService.generateQueue;
 
-test.skip('does not call makePayment or refundPayment when paymentQueue is empty', () => {
+jest.mock("../src/queueService");
+jest.mock("../src/paymentService");
+
+beforeEach(() => jest.resetAllMocks());
+
+// const spyGenerateQueue = jest.spyOn(queueService, 'generateQueue');
+// const spyMakePayment = jest.spyOn(paymentService, 'makePayment');
+// const spyRefundPayment = jest.spyOn(paymentService, 'refundPayment');
+
+describe("Test processPayments", () => {
+  it("should not call makePayment or refundPayment when paymentQueue is empty", () => {
+    generateQueue.mockImplementation(() => []);
+    expect(processPayments()).toBe(undefined);
+    expect(makePayment).toBeCalledTimes(0);
+    expect(refundPayment).toBeCalledTimes(0);
+  });
+  it('should call makePayment when next item in paymentQueue is positive', () => {
+    generateQueue.mockImplementation(() => [1]);
+    expect(processPayments()).toBe(undefined);
+    expect(makePayment).toBeCalledTimes(1);
+    expect(refundPayment).toBeCalledTimes(0);
+  });
+  it('should call refundPayment when next item in paymentQueue is negative', () => {
+    generateQueue.mockImplementation(() => [-1]);
+    expect(processPayments()).toBe(undefined);
+    expect(makePayment).toBeCalledTimes(0);
+    expect(refundPayment).toBeCalledTimes(1);
+  });
 });
 
-test.skip('calls makePayment when next item in paymentQueue is positive', () => {
-});
-
-test.skip('calls refundPayment when next item in paymentQueue is negative', () => {
-});
+// mock generateq, paymentq empty, no while loop
+// q > 0 , returns output
+// q = 1 , c
+// calls only make
+//
+// mock paymentservice and queueservice
