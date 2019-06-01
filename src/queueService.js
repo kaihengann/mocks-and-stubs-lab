0ef896e3950a457c8ae0f62a374df6c539df3e7b
kaihengann <annkaiheng@gmail.com>
2019-06-01 12:46:18 +0800

    Add comments

--- src/queueService.js
+++ src/queueService.js
@@ -1,12 +1,15 @@
 const math = require("mathjs");
 
+// returns an array with random length of 1-10 and elements from -20 to 50
 const generateQueue = () => {
   const randomInteger = math.randomInt(1, 10);
   const output = Array(randomInteger)
-    .fill()
+    .fill() // fills array with undefined
     .map(number => math.randomInt(-20, 50));
 
   return output;
 };
 
-module.exports = generateQueue;
+module.exports = {
+  generateQueue
+}
