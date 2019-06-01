6a939ef3b7aca9034d5e407b8aadb2b1d53f095c
kaihengann <annkaiheng@gmail.com>
2019-06-01 12:45:45 +0800

    Add comments and curly braces for generateQueue

--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 const { makePayment, refundPayment } = require("./paymentService");
-const generateQueue = require("./queueService");
+const { generateQueue } = require("./queueService");
 
+// Generates random queue and prints amount to pay or refund
 const processPayments = () => {
   const paymentQueue = generateQueue();
 
   while (paymentQueue.length > 0) {
-    const currentItem = paymentQueue.shift();
+    const currentItem = paymentQueue.shift(); // removes first item from queue
     if (currentItem >= 0) {
       makePayment(currentItem);
     } else {
@@ -15,6 +16,6 @@ const processPayments = () => {
 };
 
 // uncomment the next line and run: node src/main.js a few times to see how this function works
-// processPayments(); 
+// processPayments();
 
 module.exports = processPayments;
