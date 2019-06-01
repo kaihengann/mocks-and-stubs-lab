75ca1602ba4c0db96da36c1e535db4af18d07d7e
kaihengann <annkaiheng@gmail.com>
2019-06-01 12:42:55 +0800

    Modify variable names

--- test/queueService.test.js
+++ test/queueService.test.js
@@ -1,24 +1,24 @@
-const generateQueue = require("../src/queueService");
+const { generateQueue } = require("../src/queueService");
 const math = require("mathjs");
 
-const mockedRandomInt = jest.spyOn(math, "randomInt");
+const spyRandomInt = jest.spyOn(math, "randomInt");
 beforeEach(() => {
   jest.resetAllMocks();
 });
 
 describe("Test generateQueue", () => {
   it("should return [3, 3, 3] when randomInteger and number are both positive", () => {
-    mockedRandomInt.mockImplementation(() => 3);
+    spyRandomInt.mockImplementation(() => 3);
     expect(generateQueue()).toEqual([3, 3, 3]);
   });
   it("should return [-20, -20, -20] when randomInteger is positive and number is negative", () => {
-    mockedRandomInt.mockImplementation(() => -20);
-    mockedRandomInt.mockImplementationOnce(() => 3);
+    spyRandomInt.mockImplementation(() => -20);
+    spyRandomInt.mockImplementationOnce(() => 3);
     expect(generateQueue()).toEqual([-20, -20, -20]);
   });
   it("should call math.randomInt twice when randomInteger and number are both 1", () => {
-    mockedRandomInt.mockImplementation(() => 1);
+    spyRandomInt.mockImplementation(() => 1);
     expect(generateQueue()).toEqual([1]);
-    expect(mockedRandomInt).toBeCalledTimes(2);
+    expect(spyRandomInt).toBeCalledTimes(2);
   });
 });
