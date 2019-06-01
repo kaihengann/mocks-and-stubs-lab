ed6c2c6e2e5507995fd2b5477cafaf92f6b407f7
kaihengann <annkaiheng@gmail.com>
2019-06-01 13:24:37 +0800

    Add more tests

--- test/main.test.js
+++ test/main.test.js
@@ -10,10 +10,6 @@ jest.mock("../src/paymentService");
 
 beforeEach(() => jest.resetAllMocks());
 
-// const spyGenerateQueue = jest.spyOn(queueService, 'generateQueue');
-// const spyMakePayment = jest.spyOn(paymentService, 'makePayment');
-// const spyRefundPayment = jest.spyOn(paymentService, 'refundPayment');
-
 describe("Test processPayments", () => {
   it("should not call makePayment or refundPayment when paymentQueue is empty", () => {
     generateQueue.mockImplementation(() => []);
@@ -21,23 +17,40 @@ describe("Test processPayments", () => {
     expect(makePayment).toBeCalledTimes(0);
     expect(refundPayment).toBeCalledTimes(0);
   });
-  it('should call makePayment when next item in paymentQueue is positive', () => {
+  it("should call makePayment when next item in paymentQueue is positive", () => {
     generateQueue.mockImplementation(() => [1]);
     expect(processPayments()).toBe(undefined);
     expect(makePayment).toBeCalledTimes(1);
     expect(refundPayment).toBeCalledTimes(0);
   });
-  it('should call refundPayment when next item in paymentQueue is negative', () => {
+  it("should only call makePayment when all items in paymentQueue is positive", () => {
+    generateQueue.mockImplementation(() => [2, 2, 2, 2]);
+    expect(processPayments()).toBe(undefined);
+    expect(makePayment).toBeCalledTimes(4);
+    expect(refundPayment).toBeCalledTimes(0);
+  });
+  it("should call refundPayment when next item in paymentQueue is negative", () => {
     generateQueue.mockImplementation(() => [-1]);
     expect(processPayments()).toBe(undefined);
     expect(makePayment).toBeCalledTimes(0);
     expect(refundPayment).toBeCalledTimes(1);
   });
+  it("should only call refundPayment when next item in paymentQueue is negative", () => {
+    generateQueue.mockImplementation(() => [-2, -2, -2, -2]);
+    expect(processPayments()).toBe(undefined);
+    expect(makePayment).toBeCalledTimes(0);
+    expect(refundPayment).toBeCalledTimes(4);
+  });
+  it("should call makePayment when next item in paymentQueue is zero", () => {
+    generateQueue.mockImplementation(() => [0]);
+    expect(processPayments()).toBe(undefined);
+    expect(makePayment).toBeCalledTimes(1);
+    expect(refundPayment).toBeCalledTimes(0);
+  });
+  it("should call both makePayment and refundPayment when paymentQueue has both positive and negative items", () => {
+    generateQueue.mockImplementation(() => [-2, 2, -2, 2]);
+    expect(processPayments()).toBe(undefined);
+    expect(makePayment).toBeCalledTimes(2);
+    expect(refundPayment).toBeCalledTimes(2);
+  });
 });
-
-// mock generateq, paymentq empty, no while loop
-// q > 0 , returns output
-// q = 1 , c
-// calls only make
-//
-// mock paymentservice and queueservice
